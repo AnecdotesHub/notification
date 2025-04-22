bdf7fc6290a8b274897fec8acbf1d3d8cb99effe
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-22 05:24:55 +0300

    Add Cors configuration

---
+++ src/Jevstafjev.Anecdotes.Notification/Jevstafjev.Anecdotes.Notification/Definitions/Cors/CorsDefinition.cs
@@ -0,0 +1,39 @@
+﻿using Jevstafjev.Anecdotes.Notification.Definitions.Base;
+
+namespace Jevstafjev.Anecdotes.Notification.Definitions.Cors;
+
+public class CorsDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        var origins = builder.Configuration.GetSection("Cors").GetSection("Origins").Value?.Split(',');
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(AppData.PolicyCorsName, policyBuilder =>
+            {
+                policyBuilder.AllowAnyHeader();
+                policyBuilder.AllowAnyMethod();
+
+                if (origins is not { Length: > 0 })
+                {
+                    return;
+                }
+
+                if (origins.Contains("*"))
+                {
+                    policyBuilder.AllowAnyHeader();
+                    policyBuilder.AllowAnyMethod();
+                    policyBuilder.SetIsOriginAllowed(host => true);
+                    policyBuilder.AllowCredentials();
+                }
+                else
+                {
+                    foreach (var origin in origins)
+                    {
+                        policyBuilder.WithOrigins(origin);
+                    }
+                }
+            });
+        });
+    }
+}
