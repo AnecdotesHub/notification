7cefe55b6a08571f4d3846f450fab265a87ac296
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-22 05:25:05 +0300

    Create ContainerDefinition.cs

---
+++ src/Jevstafjev.Anecdotes.Notification/Jevstafjev.Anecdotes.Notification/Definitions/DependencyContainer/ContainerDefinition.cs
@@ -0,0 +1,18 @@
+﻿using Jevstafjev.Anecdotes.Notification.Application.Services;
+using Jevstafjev.Anecdotes.Notification.Definitions.Base;
+using SendGrid;
+
+namespace Jevstafjev.Anecdotes.Notification.Definitions.DependencyContainer;
+
+public class ContainerDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddTransient<IEmailService, EmailService>();
+        builder.Services.AddSingleton(provider =>
+        {
+            var configuration = provider.GetRequiredService<IConfiguration>();
+            return new SendGridClient(configuration["SendGrid:ApiKey"]);
+        });
+    }
+}
